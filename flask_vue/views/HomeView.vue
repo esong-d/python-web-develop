bcf2dbbb19d741b2abb41cc7f7a52a759355be80
easong.deng <easong.deng@tec-do.com>
2024-06-12 12:26:37 +0800

    update files

---
+++ flask_vue/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>home</h1>
+    <HelloWorld></HelloWorld>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+    name: 'homeView', 
+    components: {
+        HelloWorld
+    }
+}
+</script>
---
+++ flask_vue/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>login</h1>
+    <HelloWorld></HelloWorld>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue';
+
+    export default {
+        name: 'loginView',
+        components: {
+            HelloWorld
+        }
+    }
+</script>
