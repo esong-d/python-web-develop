6be46daa0ccea5a44c7f419a5278d77a836fb554
easong <2821453327@qq.com>
2024-06-18 17:21:49 +0800

    update uwsgi config

--- flask_learn/app.py
+++ flask_learn/app.py
@@ -1,6 +1,7 @@
 # -*- encoding = utf-8 -*-
 from flask import Flask
 from flask_cors import CORS
+from gevent import pywsgi
 
 app = Flask(__name__)
 CORS(app)
@@ -16,4 +17,9 @@ from model import *
 
 
 if __name__ == '__main__':
+    # 开发环境启动程序(单线程)
+    # app.run(host='127.0.0.1', port=9090)
+    # 生产环境启动程序(多线程)
+    server = pywsgi.WSGIServer(('127.0.0.1', 9090), app)
+    server.serve_forever()
     app.run(host='127.0.0.1', port=9090)
